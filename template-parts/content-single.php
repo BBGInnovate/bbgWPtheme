23288e0407d077907f4fb55b8b98a60fac937fbc
Brian Williamson <brianwilliamson@brians-mbp.home>
2015-12-15 07:22:24 -0500

    Commented out a full-width banner experiment.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,31 +11,58 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+
+
+
+
 <?php
+	/*
 	if (has_post_thumbnail()) {
-		echo '<div class="single-post-thumbnail clear usa-single_post_thumbnail">';
-		echo the_post_thumbnail('large-thumb');
-		echo '</div>';
+		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large-thumb' );
+		$url = $thumb['0'];
+			<section class="usa-bbg-banner" style="background-image:url(<?php echo $url; ?>)">
+				<div class="usa-grid">
+					<a href="#">
+					</a>
+					<div class="usa-bbg-banner-box">
+						<h1 class="usa-bbg-banner-site-title"><?php bloginfo( 'name' ); ?></h1>
+						<?php $description = get_bloginfo( 'description', 'display' );
+						if ( $description || is_customize_preview() ) : ?>
+							<h3 class="usa-bbg-banner-site-description"><?php echo $description; ?></h3>
+						<?php endif; ?>
+					</div>
+				</div>
+			</section>
 	}
+	*/
 ?>
+	<div class="usa-grid">
 
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php bbginnovate_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
+		<header class="entry-header">
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bbginnovate' ),
-				'after'  => '</div>',
-			) );
+			if (has_post_thumbnail()) {
+				echo '<div class="single-post-thumbnail clear usa-single_post_thumbnail usa-banner">';
+				echo the_post_thumbnail('large-thumb');
+				echo '</div>';
+			}
 		?>
-	</div><!-- .entry-content -->
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+			<div class="entry-meta">
+				<?php bbginnovate_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bbginnovate' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	</div><!-- .usa-grid -->
 
 	<footer class="entry-footer">
 		<?php bbginnovate_entry_footer(); ?>
