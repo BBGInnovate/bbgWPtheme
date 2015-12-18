cf1ae9aacf801fd82f052a155165227dc23b1b0e
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-12-18 16:55:55 -0500

    Switching from content to content-excerpt... may switch back at some point.

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ get_header(); ?>
 
 				<?php if ( is_home() && ! is_front_page() ) : ?>
 					<header>
-						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						<h4 class="page-title screen-reader-text"><?php single_post_title(); ?></h4>
 					</header>
 				<?php endif; ?>
 
@@ -36,7 +36,7 @@ get_header(); ?>
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 						 */
-						get_template_part( 'template-parts/content', get_post_format() );
+						get_template_part( 'template-parts/content-excerpt', get_post_format() );
 					?>
 
 				<?php endwhile; ?>
