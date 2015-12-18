4b733956ac65f871887ca99dd28fada8b18ff000
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-12-18 10:54:46 -0500

    Minor updates to author and author-bio. Having trouble with the author avatar.

--- author-bio.php
+++ author-bio.php
@@ -6,14 +6,15 @@
  */
 ?>
 <section class="usa-section">
-	<div class="usa-grid">
-		<div class="author-avatar">
+	<div class="usa-grid usa-bbg-author">
+		<div class="usa-bbg-avatar">
+			<img src="<?php echo get_avatar_url( 'user_email' ); ?>" alt="<?php echo esc_attr( get_the_author() ); ?>" class="bbg-staff-avatar usa-avatar"/>
 			<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( '', 100 ) ); ?>
 		</div>
 		
-		<h1 class="archive-title"><?php printf( '%s', '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' ); ?></h1>
+		<h1 class="usa-bbg-author-name"><?php printf( '%s', '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' ); ?></h1>
 
-		<div class="author-description">
+		<div class="usa-bbg-author-description">
 				<?php 
 					/* ODDI CUSTOM: add twitter handle to bio */
 					$twitterHandle = get_the_author_meta( 'twitterHandle' );
@@ -28,7 +29,7 @@
 						echo '<div id="authorContact"><a href="//www.twitter.com/' . $twitterHandle. '">@' . $twitterHandle . '</a> ' . $website .'</div>';
 					}
 				?>
-			<p class="author-bio">
+			<p class="usa-bbg-author-bio">
 				<?php if ( get_the_author_meta( 'description' ) ) : ?>
 					<?php the_author_meta( 'description' ); ?>
 				<?php endif; ?>
--- author.php
+++ author.php
@@ -35,6 +35,7 @@ get_header(); ?>
 
 
 			<?php else : ?>
+				<?php get_template_part( 'author-bio' ); ?>
 				<?php get_template_part( 'content', 'none' ); ?>
 			<?php endif; ?>
 
