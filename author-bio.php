6219d699d29caccda01e43f16c910d7a3dffb2c7
Brian Williamson <brianwilliamson@brians-mbp.home>
2015-12-18 21:29:38 -0500

    Adding occupation to the author bio.

--- author-bio.php
+++ author-bio.php
@@ -18,6 +18,8 @@
 						/* ODDI CUSTOM: add twitter handle to bio */
 						$twitterHandle = get_the_author_meta( 'twitterHandle' );
 						$website = get_the_author_meta( 'user_url' );
+						$occupation = get_the_author_meta( 'occupation' );
+						echo '<div class="usa-bbg-author-occupation">' . $occupation . '</div>';
 
 						if ( $website && $website != '' ) {
 							$website='<span class="sep"> | </span><a href="' . $website . '">' . $website . '</a>';
@@ -25,14 +27,14 @@
 
 						if ( $twitterHandle && $twitterHandle != '' ) {
 							$twitterHandle=str_replace("@", "", $twitterHandle);
-							echo '<div id="authorContact"><a href="//www.twitter.com/' . $twitterHandle. '">@' . $twitterHandle . '</a> ' . $website .'</div>';
+							echo '<div class="usa-bbg-author-contact"><a href="//www.twitter.com/' . $twitterHandle. '">@' . $twitterHandle . '</a> ' . $website .'</div>';
 						}
 					?>
-				<p class="usa-bbg-author-bio">
+				<div class="usa-bbg-author-bio">
 					<?php if ( get_the_author_meta( 'description' ) ) : ?>
 						<?php the_author_meta( 'description' ); ?>
 					<?php endif; ?>
-				</p>
+				</div>
 				<div class='clearAll'></div>
 			</div>
 			<!-- .author-description -->
