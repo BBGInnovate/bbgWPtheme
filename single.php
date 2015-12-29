63aa6b026147389f979a42704387b538d9cbb2e3
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-12-29 16:12:42 -0500

    Added a container div to help with with positioning of post-footer stuff.

--- single.php
+++ single.php
@@ -16,15 +16,16 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+			<div class="bbg-post-footer">
+				<?php the_post_navigation(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+			</div>
 		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
