21c4ed70756102b332dd48cc472b1802c87fca13
Brian Williamson <brianwilliamson@Brians-MacBook-Pro.local>
2015-12-30 09:00:40 -0500

    MINOR CSS adjustment for the main-nav and toggle button.

--- page-staff.php
+++ page-staff.php
@@ -30,9 +30,6 @@ get_header();
 
 					<div class="bbg-staff-profile">
 						<a href="<?php echo $authorPath ?>">
-							<!--
-							<img src="<?php echo get_avatar_url( $user->user_email ); ?>" alt="<?php echo $authorName; ?>" class="bbg-staff-avatar usa-avatar"/>
-							-->
 							<?php echo get_avatar( $user->user_email , apply_filters( 'change_avatar_css', 150) ); ?>
 						</a>
 
