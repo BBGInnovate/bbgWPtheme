3be714dc064409f9bf4194a89daa8d3ac50ca5ba
Brian Williamson <brianwilliamson@brianw.local>
2016-02-17 11:53:40 -0500

    Centering and sizing comments div.

--- single.php
+++ single.php
@@ -40,17 +40,13 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<div class="bbg_article-footer">
-				<div class="usa-grid">
-					<?php /*the_post_navigation();*/ ?> <!-- Hiding previous/next navigation buttons :: GIGI -->
-
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
-				</div>
+			<div class="bbg_article-footer usa-grid">
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
 			</div>
 		<?php endwhile; // End of the loop. ?>
 
