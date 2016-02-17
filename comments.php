6974338875c9e8a84cf4c4cc31e23f48e7cb3749
Gigi <gigi.frias@gmail.com>
2016-02-17 18:01:58 -0500

    Updates to comments

--- comments.php
+++ comments.php
@@ -53,6 +53,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ul',
 					'short_ping' => true,
+					'avatar_size' => 150
 				) );
 			?>
 		</ul><!-- .comment-list -->
