dca8cbcebc01b42c3d60b6f61802ae92b6ef768a
Brian Williamson <brianwilliamson@brianw.home>
2016-02-17 23:28:33 -0500

    Minor fuego styles.

--- page-fuego.php
+++ page-fuego.php
@@ -30,9 +30,9 @@ get_header();
 					<div class="bbg-avatar__container bbg-team__icon">
 						<div class="bbg-avatar bbg-team__icon__image" style="background-image: url(https://africa.rizing.org/wp-content/uploads/2015/10/trending-150x150.png);"></div>
 					</div>
-					<div class="usa-intro bbg-page__header-description bbg-team__text">
+					<div class="usa-intro bbg-page__header-text bbg-team__text">
 						<h1 class="bbg-page__header-title">Fuego</h1>
-						<h3 class="usa-font-lead">An auto-generated stream of links and conversations from our community — powered by <a href='https://github.com/niemanlab/openfuego' target='_blank'>Fuego</a></h3>
+						<h3 class="usa-font-lead bbg-page__header-description">An auto-generated stream of links and conversations from our community — powered by <a href='https://github.com/niemanlab/openfuego' target='_blank'>Fuego</a></h3>
 					</div>
 				</header><!-- .page-header -->
 			</div>
