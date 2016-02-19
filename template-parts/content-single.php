956d06abf71a07b3baea97e317236108f11a78f3
Gigi Frias <gigi.frias@gmail.com>
2016-02-19 12:01:09 -0500

    update to author id query

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -120,7 +120,7 @@ $fbUrl="//www.facebook.com/sharer/sharer.php?u=" . urlencode( get_permalink() );
 
 	</div><!-- .usa-grid -->
 
-	<?php bbg_post_author_bottom_card(get_the_author_id()); ?>
+	<?php bbg_post_author_bottom_card(get_the_author_meta('ID')); ?>
 
 	<!-- <footer class="entry-footer bbg__article-footer">
 		<?php bbginnovate_entry_footer(); ?>
