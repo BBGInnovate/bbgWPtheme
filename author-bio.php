2a4973852d939c4962e94a68bec03b008e9ad568
Brian Williamson <brianwilliamson@brianw.home>
2016-02-23 23:00:29 -0500

    author bio grid

--- author-bio.php
+++ author-bio.php
@@ -111,7 +111,7 @@ if ( $twitterHandle && $twitterHandle != '' ) {
 
 if ( count( $projects ) ) {
 	$maxProjectsToShow=5;
-	echo '<div class="usa-width-one-third">';
+	echo '<div class="usa-width-one-third bbg-author-projects">';
 	echo '<h6 class="bbg-label small">Projects:</h2>';
 	echo '<ul>';
 	for ( $i=0; $i<min( $maxProjectsToShow, count( $projects ) ); $i++ ) {
