900470c0d50c25c152dcbe2a1bef3c225e6cfe7f
Joe Flowers <jflowers45@gmail.com>
2016-02-25 18:59:47 -0500

    simplify bad css demo

--- demo-badsearch.php
+++ demo-badsearch.php
@@ -1,17 +1,21 @@
 <html lang="en-US">
 <head>
-        <title>Innovation @ BBG &#8211; Driving innovation in global news</title>
-        <link rel='stylesheet' id='bbginnovate-style-css'  href='http://54.221.228.104/wp-content/themes/bbginnovate/style.css?ver=4.4.2' type='text/css' media='all' />
-        <link rel='stylesheet' id='bbginnovate-style-fonts-css'  href='http://54.221.228.104/wp-content/themes/bbginnovate/css/bbg-fonts.css?ver=4.4.2' type='text/css' media='all' />
+        <title>Innovation @ BBG</title>
+        <style>
+			a:hover {
+			  text-decoration: underline;
+			  color:green;
+			}
+			.usa-form-note + * {
+			  clear: both;
+			}
+		</style>
 </head>
 <body >
 <a href="http://google.com" style="font-size:48px;">google</a>
-<form role="search" method="get" class="search-form" action="http://54.221.228.104/">
-        <label>
-                <span class="screen-reader-text">Search for:</span>
-                <input type="search" class="search-field" placeholder="Search &hellip;" value="" name="s" title="Search for:" />
-        </label>
-        <input type="submit" class="search-submit" value="Search" />
+<form role="search" method="get" action="http://54.221.228.104/">
+	<input type="search" name="s"/>
+	<input type="submit" value="Search" />
 </form>
 </body>
 </html>
