b12f01debf0dac411d42d540c028b7f4d385feca
Brian Williamson <bbg.visualjournalist@gmail.com>
2016-03-01 12:02:16 -0500

    Changing search input type to "text" to prevent need for double tap.

--- searchform.php
+++ searchform.php
@@ -16,8 +16,8 @@
 
 <form class="usa-search usa-search-small" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<div role="search">
-		<label class="usa-sr-only" for="search-field-small">Search big</label>
-		<input type="search" class="search-field"
+		<label class="usa-sr-only" for="search-field-small">Search</label>
+		<input type="text" class="search-field"
 			id="search-field-small" 
 			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
 			value="<?php echo get_search_query() ?>" 
