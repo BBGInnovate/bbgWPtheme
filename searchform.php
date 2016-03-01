a1e8620b051f0e41a527e8a4432df9b42e2621bb
Brian Williamson <bbg.visualjournalist@gmail.com>
2016-03-01 11:21:52 -0500

    Updating search form to match USWDS markup.

--- searchform.php
+++ searchform.php
@@ -1,3 +1,5 @@
+<?php 
+/*
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <label>
         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
@@ -8,4 +10,21 @@
     </label>
     <input type="submit" class="search-submit"
         value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+</form>
+*/
+?>
+
+<form class="usa-search usa-search-small" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<div role="search">
+		<label class="usa-sr-only" for="search-field-small">Search big</label>
+		<input type="search" class="search-field"
+			id="search-field-small" 
+			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+			value="<?php echo get_search_query() ?>" 
+			name="s"
+			title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+		<button type="submit">
+			<span class="usa-sr-only">Search</span>
+		</button>
+	</div>
 </form>
