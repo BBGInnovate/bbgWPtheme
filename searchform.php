7d6165b7062ced52084eb884a32417aa189960f1
Brian Williamson <bbg.visualjournalist@gmail.com>
2016-03-01 12:13:35 -0500

    Fixing the styling of the search bar when the input type = "text".

--- searchform.php
+++ searchform.php
@@ -1,23 +1,7 @@
-<?php 
-/*
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <label>
-        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-        <input type="text" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
-            value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
-    <input type="submit" class="search-submit"
-        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-</form>
-*/
-?>
-
 <form class="usa-search usa-search-small" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<div role="search">
 		<label class="usa-sr-only" for="search-field-small">Search</label>
-		<input type="text" class="search-field"
+		<input type="text" class="search-field usa-search-input"
 			id="search-field-small" 
 			placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
 			value="<?php echo get_search_query() ?>" 
