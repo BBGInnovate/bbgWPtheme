44763b20ea483eb8f0978caa60d4c6be9e8d9b7a
Gigi Frias <gigi.frias@gmail.com>
2016-03-02 12:33:19 -0500

    fixing dumb quote...dumb quotes!

--- page-staff.php
+++ page-staff.php
@@ -17,7 +17,7 @@ get_header();
 			<section class="usa-section usa-grid-full bbg-staff__roster">
 				<header class="bbg-page__header usa-grid">
 					<h1 class="bbg-page__header-title">STAFF</h1>
-					<h3 class="bbg-page__header-description">ODDI's designers, developers and storytellers help drive USIM digital projects.</h3>
+					<h3 class="bbg-page__header-description">ODDI’s designers, developers and storytellers help drive USIM digital projects.</h3>
 				</header>
 				<div class="usa-grid">
 					<?php
