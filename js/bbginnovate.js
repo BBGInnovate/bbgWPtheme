526b4a639ffa61f2c9e8be102b2dd9d898c26d50
Joe Flowers <jflowers45@gmail.com>
2016-03-14 18:01:43 -0400

    js for JAVA post

--- js/bbginnovate.js
+++ js/bbginnovate.js
@@ -1,172 +1,194 @@
 jQuery(document).ready(function() { 
+  function initJavaSelect() {
+    //for an example of this in action, see http://innovation.bbg.gov/blog/bbgs-new-java-based-apps-are-built-for-very-low-end-or-feature-phones/
+    entities = [{
+        "value": "VOA",
+        "display": "VOA News"
+    }, {
+        "value": "Alhurra",
+        "display": "Alhurra TV"
+    }, {
+        "value": "Marti",
+        "display": "Martí Noticias"
+    }, {
+        "value": "RFERL",
+        "display": "Radio Free Europe/Radio Liberty"
+    }, {
+        "value": "RFA",
+        "display": "Radio Free Asia"
+    }];
   
-  //for an example of this in action, see http://innovation.bbg.gov/blog/bbgs-new-java-based-apps-are-built-for-very-low-end-or-feature-phones/
-  function initAppSelect() {
 
-        select1 = [{
-            "value": "VOANEWS",
-            "display": "VOA News"
-        }, {
-            "value": "ALHURRA",
-            "display": "Alhurra TV"
-        }, {
-            "value": "SAWA",
-            "display": "Radio Sawa"
-        }, {
-            "value": "MARTI",
-            "display": "Martí Noticias"
-        }, {
-            "value": "RFERL",
-            "display": "Radio Free Europe/Radio Liberty"
-        }, {
-            "value": "RFA",
-            "display": "Radio Free Asia"
-        }];
+    stores = ["binu", "opera", "getjar"];
 
-        entityDetails = {
-            "VOANEWS": [{
-                "language": "Amharic",
-                "URL": "http://www.getjar.mobi/mobile/848255/VOA-Amharic-for-Java-Phones"
-            }, {
-                "language": "Chinese Simplified",
-                "URL": "http://www.getjar.mobi/mobile/848265/VOA-Chinese-Simplified-for-Java-Phones"
-            }, {
-                "language": "Chinese Traditional",
-                "URL": "http://www.getjar.mobi/mobile/848266/VOA-Chinese-Traditional-for-Java-Phones"
-            }, {
-                "language": "English",
-                "URL": "http://www.getjar.mobi/mobile/847025/VOA-News-for-Java-Phones"
-            }, {
-                "language": "French",
-                "URL": "http://www.getjar.mobi/mobile/848176/VOA-French-for-Java-Phones"
-            }, {
-                "language": "Indonesian",
-                "URL": "http://www.getjar.mobi/mobile/848263/VOA-Indonesian-for-Java-Phones"
-            }, {
-                "language": "Korean",
-                "URL": "http://www.getjar.mobi/mobile/848269/VOA-Korean-for-Java-Phones"
-            }, {
-                "language": "Persian",
-                "URL": "http://www.getjar.mobi/mobile/848262/VOA-Persian-for-Java-Phones"
-            }, {
-                "language": "Portuguese",
-                "URL": "http://www.getjar.mobi/mobile/848253/VOA-Portuguese-for-Java-Phones"
-            }, {
-                "language": "Russian",
-                "URL": "http://www.getjar.mobi/mobile/848272/VOA-Russian-for-Java-Phones"
-            }, {
-                "language": "Serbian",
-                "URL": "http://www.getjar.mobi/mobile/848274/VOA-Serbian-for-Java-Phones"
-            }, {
-                "language": "Spanish",
-                "URL": "http://www.getjar.mobi/mobile/848260/VOA-Spanish-for-Java-Phones"
-            }, {
-                "language": "Swahili",
-                "URL": "http://www.getjar.mobi/mobile/848254/VOA-Swahili-for-Java-Phones"
-            }, {
-                "language": "Thai",
-                "URL": "http://www.getjar.mobi/mobile/848264/VOA-Thai-for-Java-Phones"
-            }, {
-                "language": "Turkish",
-                "URL": "http://www.getjar.mobi/mobile/848271/VOA-Turkish-for-Java-Phones"
-            }, {
-                "language": "Ukrainian",
-                "URL": "http://www.getjar.mobi/mobile/848273/VOA-Ukrainian-for-Java-Phones"
-            }, {
-                "language": "Vietnamese",
-                "URL": "http://www.getjar.mobi/mobile/848268/VOA-Vietnamese-for-Java-Phones"
-            }],
+    links={};
+    links["Alhurra"]={};
+    links["Alhurra"]["binu"]={};
+    links["Alhurra"]["getjar"]={};
+    links["Alhurra"]["opera"]={};
+    links["Alhurra"]["binu"]["multi"]="http://m.binu.com/alh/";
+    links["Alhurra"]["getjar"]["multi"]="http://www.getjar.mobi/mobile/848171/-Alhurra-for-Java-Phones";
+    links["Alhurra"]["opera"]["multi"]="http://java.apps.opera.com/en_us/alhurra_for_java_phones.html?dm=1&multi=1";
 
-            "ALHURRA": [{
-                "language": "Arabic",
-                "URL": "http://www.getjar.com/mobile/848171/-Alhurra-for-Java-Phones/"
-            }],
+    links["Marti"]={};
+    links["Marti"]["binu"]={};
+    links["Marti"]["getjar"]={};
+    links["Marti"]["opera"]={};
+    links["Marti"]["binu"]["multi"]="http://m.binu.com/mar/";
+    links["Marti"]["getjar"]["multi"]="http://www.getjar.mobi/mobile/848168/OCB-Mart-Noticias-for-Java-Phones";
+    links["Marti"]["opera"]["multi"]="http://java.apps.opera.com/en_us/marti_noticias_for_java_phones.html?dm=1&multi=1";
 
-            "SAWA": [{
-                "language": "Arabic",
-                "URL": "http://www.getjar.com/mobile/851300/-Radio-Sawa-for-Java-Phones/"
-            }],
+    links["RFA"]={};
+    links["RFA"]["binu"]={};
+    links["RFA"]["getjar"]={};
+    links["RFA"]["opera"]={};
+    links["RFA"]["binu"]["multi"]="http://m.binu.com/rfa/";
+    links["RFA"]["getjar"]["Chinese_Simplified"]="http://www.getjar.mobi/mobile/848351/RFA-Chinese-Simplified-for-Java-Phones";
+    links["RFA"]["getjar"]["Chinese_Traditional"]="http://www.getjar.mobi/mobile/848352/RFA-Chinese-Traditional-for-Java-Phones";
+    links["RFA"]["getjar"]["English"]="http://www.getjar.mobi/mobile/848349/RFA-English-for-Java-Phones";
+    links["RFA"]["getjar"]["Korean"]="http://www.getjar.mobi/mobile/848353/RFA-Korean-for-Java-Phones";
+    links["RFA"]["getjar"]["Vietnamese"]="http://www.getjar.mobi/mobile/848354/RFA-Vietnamese-for-Java-Phones";
+    links["RFA"]["opera"]["multi"]="http://java.apps.opera.com/en_us/radio_free_asia_rfa_for_java_phones.html?dm=1&multi=1&set_lang=en";
 
-            "MARTI": [{
-                "language": "Spanish",
-                "URL": "http://www.getjar.mobi/mobile/848168/OCB-Mart-Noticias-for-Java-Phones"
-            }, ],
+    links["RFERL"]={};
+    links["RFERL"]["binu"]={};
+    links["RFERL"]["getjar"]={};
+    links["RFERL"]["opera"]={};
+    links["RFERL"]["binu"]["multi"]="http://m.binu.com/rfe/";
+    links["RFERL"]["getjar"]["Belarusian"]="http://www.getjar.mobi/mobile/848341/RFERL-Belarusian-for-Java-Phones";
+    links["RFERL"]["getjar"]["English"]="http://www.getjar.mobi/mobile/848340/RFERL-English-for-Java-Phones";
+    links["RFERL"]["getjar"]["Persian"]="http://www.getjar.mobi/mobile/848343/RFERL-Persian-for-Java-Phones";
+    links["RFERL"]["getjar"]["Russian"]="http://www.getjar.mobi/mobile/848344/RFERL-Russian-for-Java-Phones";
+    links["RFERL"]["getjar"]["Ukrainian"]="http://www.getjar.mobi/mobile/848345/RFERL-Ukrainian-for-Java-Phones";
+    links["RFERL"]["opera"]["multi"]="http://java.apps.opera.com/en_us/rferl_for_java_phones.html?dm=1&multi=1&set_lang=en";
 
-            "RFERL": [{
-                "language": "Belarusian",
-                "URL": "http://www.getjar.mobi/mobile/848341/RFERL-Belarusian-for-Java-Phones"
-            }, {
-                "language": "English",
-                "URL": "http://www.getjar.mobi/mobile/848340/RFERL-English-for-Java-Phones"
-            }, {
-                "language": "Persian",
-                "URL": "http://www.getjar.mobi/mobile/848343/RFERL-Persian-for-Java-Phones"
-            }, {
-                "language": "Russian",
-                "URL": "http://www.getjar.mobi/mobile/848344/RFERL-Russian-for-Java-Phones"
-            }, {
-                "language": "Ukrainian",
-                "URL": "http://www.getjar.mobi/mobile/848345/RFERL-Ukrainian-for-Java-Phones"
-            }],
+    links["VOA"]={};
+    links["VOA"]["binu"]={};
+    links["VOA"]["getjar"]={};
+    links["VOA"]["opera"]={};
+    links["VOA"]["binu"]["multi"]="http://m.binu.com/voa/";     
+    links["VOA"]["getjar"]["English"] = "http://www.getjar.mobi/mobile/847025/VOA-News-for-Java-Phones";
+    links["VOA"]["getjar"]["French"] = "http://www.getjar.mobi/mobile/848176/VOA-French-for-Java-Phones";
+    links["VOA"]["getjar"]["Indonesian"] = "http://www.getjar.mobi/mobile/848263/VOA-Indonesian-for-Java-Phones";
+    links["VOA"]["getjar"]["Korean"] = "http://www.getjar.mobi/mobile/848269/VOA-Korean-for-Java-Phones";
+    links["VOA"]["getjar"]["Persian"] = "http://www.getjar.mobi/mobile/848262/VOA-Persian-for-Java-Phones";
+    links["VOA"]["getjar"]["Portuguese"] = "http://www.getjar.mobi/mobile/848253/VOA-Portuguese-for-Java-Phones";
+    links["VOA"]["getjar"]["Russian"] = "http://www.getjar.mobi/mobile/848272/VOA-Russian-for-Java-Phones";
+    links["VOA"]["getjar"]["Serbian"] = "http://www.getjar.mobi/mobile/848274/VOA-Serbian-for-Java-Phones";
+    links["VOA"]["getjar"]["Spanish"] = "http://www.getjar.mobi/mobile/848260/VOA-Spanish-for-Java-Phones";
+    links["VOA"]["getjar"]["Swahili"] = "http://www.getjar.mobi/mobile/848254/VOA-Swahili-for-Java-Phones";
+    links["VOA"]["getjar"]["Thai"] = "http://www.getjar.mobi/mobile/848264/VOA-Thai-for-Java-Phones";
+    links["VOA"]["getjar"]["Turkish"] = "http://www.getjar.mobi/mobile/848271/VOA-Turkish-for-Java-Phones";
+    links["VOA"]["getjar"]["Ukrainian"] = "http://www.getjar.mobi/mobile/848273/VOA-Ukrainian-for-Java-Phones";
+    links["VOA"]["getjar"]["Vietnamese"] = "http://www.getjar.mobi/mobile/848268/VOA-Vietnamese-for-Java-Phones";
+    links["VOA"]["getjar"]["Amharic"] = "http://www.getjar.mobi/mobile/848255/VOA-Amharic-for-Java-Phones";
+    links["VOA"]["getjar"]["Chinese Simplified"] = "http://www.getjar.mobi/mobile/848265/VOA-Chinese-Simplified-for-Java-Phones";
+    links["VOA"]["getjar"]["Chinese Traditional"] = "http://www.getjar.mobi/mobile/848266/VOA-Chinese-Traditional-for-Java-Phones";
+    links["VOA"]["opera"]["multi"]="http://java.apps.opera.com/en_us/voa_news_for_java_phones.html?dm=1&multi=1&set_lang=en";
 
-            "RFA": [{
-                "language": "Chinese Simplified",
-                "URL": "http://www.getjar.mobi/mobile/848351/RFA-Chinese-Simplified-for-Java-Phones"
-            }, {
-                "language": "Chinese Traditional",
-                "URL": "www.getjar.mobi/mobile/848352/RFA-Chinese-Traditional-for-Java-Phones"
-            }, {
-                "language": "English",
-                "URL": "http://www.getjar.mobi/mobile/848349/RFA-English-for-Java-Phones"
-            }, {
-                "language": "Korean",
-                "URL": "http://www.getjar.mobi/mobile/848353/RFA-Korean-for-Java-Phones"
-            }, {
-                "language": "Vietnamese",
-                "URL": "http://www.getjar.mobi/mobile/848354/RFA-Vietnamese-for-Java-Phones"
-            }]
-        };
 
-        /*** Populate the entity selector ****/
+
+    /*** Populate the entity selector ****/
+    var str = "";
+    for (var i = 0; i < entities.length; i++) {
+        str += "<option value=" + entities[i].value + ">" + entities[i].display + "</option>";
+    }
+    jQuery("#appSelect-java select[name=entity]").append(str);
+
+    /*** Populate the store selector ****/
+    str="";
+    for (var i = 0; i < stores.length; i++) {
+        str += "<option value=" + stores[i] + ">" + stores[i] + "</option>";
+    }
+    jQuery("#appSelect-java select[name=store]").append(str);    
+    
+
+    /*** Show the form (we keep it hidden until it has data) ****/
+    jQuery("#appSelect-java").css("display", "block");
+
+    function refreshLanguages() {
+        jQuery("select[name=language]").empty();
+        var selectedEntity= jQuery("select[name=entity]").val();
+        var selectedStore= jQuery("select[name=store]").val();
+        var languages = links[selectedEntity][selectedStore];
+
         var str = "";
-        for (var i = 0; i < select1.length; i++) {
-            str += "<option value=" + select1[i].value + ">" + select1[i].display + "</option>";
-        }
-        jQuery("#appSelect select[name=entity]").append(str);
-
-        /*** Show the form (we keep it hidden until it has data) ****/
-        console.log('show the app select form');
-        jQuery("#appSelect").css("display", "block");
-
-        jQuery("#appSelect select[name=entity]").change(function() {
-            var newEntity = jQuery(this).val();
-            jQuery("#appSelect select[name=language]").empty();
-            var newOptions = entityDetails[newEntity];
-            var str = "";
-            for (var i = 0; i < newOptions.length; i++) {
-                var o = newOptions[i];
-                str += "<option value='" + o.URL + "'>" + o.language + "</option>";
-            }
-            jQuery("#appSelect select[name=language]").append(str);
-            /*** Show the second field once it's populated ****/
-            jQuery("#languages").css("display", "block");
-        })
-
-        jQuery("#appSelect input[name=btnGo]").click(function() {
-            var entityValue = jQuery("#appSelect select[name=entity]").val();
-            var languageValue = jQuery("#appSelect select[name=language] option:selected").text();
-            var targetUrl = "";
-            for (var i = 0; i < entityDetails[entityValue].length; i++) {
-                var o = entityDetails[entityValue][i];
-                if (o.language == languageValue) {
-                    targetUrl = o.URL;
+        var hasMulti=false;
+        for (var key in languages) {
+            if (languages.hasOwnProperty(key)) {
+                if (key == "multi") {
+                    hasMulti=true;
+                } else {
+                    str += "<option value=" + key + ">" + key + "</option>";
                 }
             }
-            if (targetUrl != "") {
-                window.open(targetUrl, '_blank');
-            }
-        });
+        }
+        jQuery("select[name=language]").append(str);    
+        if (!hasMulti) {
+            jQuery("#languages").show();
+        } else {
+            jQuery("#languages").hide();
+            jQuery("select[name=language]").prop('selectedIndex',1);
+            jQuery("input[name=btnGo]").show();
+        }
     }
-    initAppSelect();
+
+    /*** when entity is changed, clear the store selection and remove language options***/
+    jQuery("select[name=entity]").change(function() {
+        var newEntity = jQuery(this).val();
+        jQuery("#appSelect-java select[name=store]").prop('selectedIndex',0);
+        jQuery("#appSelect-java select[name=language]").empty();
+        jQuery("#languages").hide();
+        jQuery("input[name=btnGo]").hide();
+
+    })
+
+    /*** when store is changed, fill in the languages ***/
+    jQuery("select[name=store]").change(function() {
+        var storeValue = jQuery("select[name=store]").val();
+        if (storeValue != "") {
+            refreshLanguages();
+        } else {
+            jQuery("#languages").hide();
+            jQuery("input[name=btnGo]").hide();
+        }
+    })
+
+    jQuery("input[name=btnGo]").click(function() {
+        var entityValue = jQuery("select[name=entity]").val();
+        var storeValue = jQuery("select[name=store]").val();
+        var languageValue = jQuery("select[name=language] option:selected").text();
+        var targetUrl = "";
+        if (links[entityValue][storeValue].hasOwnProperty("multi")) {
+            targetUrl=links[entityValue][storeValue]["multi"];
+        } else {
+            targetUrl=links[entityValue][storeValue][languageValue];
+        }
+        if (targetUrl != "") {
+            window.open(targetUrl, '_blank');
+        }
+    });
+
+    jQuery("#languages").hide();
+    jQuery("input[name=btnGo]").hide();
+
+  }
+
+  function initSmartSelect() {
+
+  }
+
+  function initSawaSelect() {
+
+  }
+
+  function initStreamerSelect() {
+
+  }
+
+  if (jQuery("#appSelect-java").length) {
+    initJavaSelect();
+  }
+  
 
 });
