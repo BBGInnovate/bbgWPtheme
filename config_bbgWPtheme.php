8356e7af896f43d2e8bf7aa8cb886ed3590a3b36
Joe Flowers <jflowers45@gmail.com>
2016-04-27 17:22:34 -0400

    testing linkedin images

--- config_bbgWPtheme.php
+++ config_bbgWPtheme.php
@@ -2,7 +2,10 @@
 
 //this file is called in header.php.  These are used for various og/twitter/social media tags
 define("DEFAULT_DESCRIPTION", "Innovation Series is the Broadcasting Board of Governors Office of Design and Digital Media Strategy's ongoing discussion on innovation in media.");
-define("DEFAULT_IMAGE", site_url() . "/wp-content/themes/bbginnovate/img/logo-agency-square.png");
+define("DEFAULT_IMAGE", site_url() . "/wp-content/uploads/2016/03/smartphone-mobile-apps.png");
+///wp-content/themes/bbginnovate/img/logo-agency-square.png
+//https://innovation.bbg.gov/wp-content/uploads/2016/03/smartphone-mobile-apps.png
+
 define("DEFAULT_TITLE",get_bloginfo('name'));
 define("DEFAULT_AUTHOR","");
 define("DEFAULT_KEYWORDS","");
